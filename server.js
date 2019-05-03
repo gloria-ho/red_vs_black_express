b0e47711e9594b1c2b0430e5d2c754847c7beda2
Gloria H <silverdragonia@gmail.com>
2019-05-03 15:31:05 -0400

    cleaning up code

--- server.js
+++ server.js
@@ -2,12 +2,10 @@
 let express = require('express');
 // initialize the express app.
 let app = express();
-
 // where to find static files to serve to the browser
 let staticFileRootFolder = './public';
 // set the local port
 const port = 3000;
-
 
 // built-in express static file functionality
 app.use(express.static(staticFileRootFolder));
